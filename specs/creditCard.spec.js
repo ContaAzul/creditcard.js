cceb4799ad3996c2224868cce08f145764fdddaa
fernahh <fernahh@gmail.com>
2016-01-23 20:28:42 -0200

    Create `validate()` method in CreditCard class.

--- specs/creditCard.spec.js
+++ specs/creditCard.spec.js
@@ -3,20 +3,31 @@ import CreditCard from '../src/creditCard.js';
 describe('CreditCard', () => {
   beforeEach(() => {
     global.creditcard = new CreditCard();
+    global.VALID_CREDIT_CARD = '4024007194756572';
+    global.INVALID_CREDIT_CARD = '00000';
   });
 
-  describe('#validadeCreditCard', () => {});
   describe('#validadeSecuryCode', () => {});
   describe('#validadeExpiryDate', () => {});
 
+  describe('#validadeCreditCard', () => {
+    it('should return true with its a VALID credit card', () => {
+      expect(creditcard.validate(VALID_CREDIT_CARD)).toBeTruthy();
+    });
+
+    it('should return false with its a VALID credit card', () => {
+      expect(creditcard.validate(INVALID_CREDIT_CARD)).toBeFalsy();
+    });
+  });
+
   describe('#getCreditCardNameByNumber', () => {
     it('should return the name of Credit Card', () => {
-      let creditCardName = creditcard.getCreditCardNameByNumber('4024007194756572');
+      let creditCardName = creditcard.getCreditCardNameByNumber(VALID_CREDIT_CARD);
       expect(creditCardName).toBe('Visa');
     });
 
     it('should return false', () => {
-      let creditCardName = creditcard.getCreditCardNameByNumber('9129310293012931');
+      let creditCardName = creditcard.getCreditCardNameByNumber(INVALID_CREDIT_CARD);
       expect(creditCardName).toBeFalsy();
     });
   });
--- src/creditCard.js
+++ src/creditCard.js
@@ -1,15 +1,24 @@
 import CreditCardList from './creditCardList';
+import Luhn from './helpers/luhn';
 
 export default class CreditCard {
   constructor() {
     this.creditcardlist = new CreditCardList();
+    this.luhn = new Luhn();
   }
 
   getCreditCardList() {
     return this.creditcardlist.getCreditCardList();
   }
 
+  validate(number) {
+    return this.luhn.validate(number);
+  }
+
   getCreditCardNameByNumber(number) {
+    if (!this.validate(number))
+      return false;
+
     let CREDIT_CARD_LIST = this.getCreditCardList();
 
     for (let i = 0; i < CREDIT_CARD_LIST.length; i++) {
