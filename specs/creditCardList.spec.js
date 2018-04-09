ef21d844ee46b0cd99795c7f0b68f20f629ae30e
Thiago Adriano <thiago.s.adriano@gmail.com>
2018-04-09 18:53:53 -0300

    Changed methods to be static and new method added

--- specs/creditCardList.spec.js
+++ specs/creditCardList.spec.js
@@ -2,13 +2,31 @@ import CreditCardList from '../src/creditCardList.js';
 
 describe('CreditCardList', () => {
   beforeEach(() => {
-    global.list = new CreditCardList();
+    global.list = CreditCardList;
   });
 
-  describe('#getCreditCardList', () => {
-    it('should return a array with cards', () => {
-      let cards = list.retrieveCreditCardList();
-      expect(cards instanceof Array).toBeTruthy();
+  describe('#CreditCardList', () => {
+    it('shall ensure that the', () => {
+      let cards = list;
+      expect(cards).toBeDefined();
+    });
+  });
+
+  describe('#count', () => {
+    it('should the list is filled', () => {
+      expect(list.count()).toBeGreaterThanOrEqual(8);
+    });
+  });
+
+  describe('#getItemByIndex', () => {
+    it('must ensure that you are returning an item', () => {
+      let item = list.getItemByIndex(2);
+      expect(item).toBeDefined();
+      expect(item.name).toEqual('Discover');
+    });
+
+    it('must guarantee that it returns null when it has no item', () => {
+      expect(list.getItemByIndex(10)).toBeNull();
     });
   });
 });
