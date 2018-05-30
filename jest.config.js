dd68c770f3012f374d06b888c29c481903235186
Carlos Roberto Gomes Junior <carlos.rberto@gmail.com>
2018-05-30 13:56:09 -0300

    added coverage threshold configs

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   coverageDirectory: '.coverage',
   collectCoverageFrom: [
-    'src/**/*.{js,jsx}',
+    'src/**/*.js',
   ],
+  coverageThreshold: {
+    global: {
+      statements: 97,
+      branches: 95,
+      functions: 100,
+      lines: 97
+    },
+  },
 };
