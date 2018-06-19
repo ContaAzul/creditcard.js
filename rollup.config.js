47ad78f9d63ad449732c835f07495bbb5ac50065
fernahh <fernahh@gmail.com>
2018-06-19 10:49:19 -0300

    Remove minified file from dist

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,10 @@
 import babel from 'rollup-plugin-babel';
-import uglify from 'rollup-plugin-uglify';
 import { version } from './package.json';
 
 const banner = `/**
  * @name creditcard.js ${version}
  * @license MIT
- * @author ContaAzul <fernahh@gmail.com> (contaazul.com)
+ * @author ContaAzul (contaazul.com)
  */`;
 
 export default {
@@ -13,20 +12,13 @@ export default {
   input: 'src/creditCard.js',
   output: {
     banner: banner,
-    file: 'dist/creditcard.min.js',
+    file: 'dist/creditcard.js',
     format: 'umd'
   },
   plugins: [
     babel({
       exclude: 'node_modules/**'
-    }),
-    (process.env.BUILD === 'production' && uglify({
-      output: {
-        comments: {
-          preserveComments: /(?:^!|@(?:name|license|author))/
-        }
-      }
-    }))
+    })
   ],
   watch: {
     exclude: 'node_modules/**'
