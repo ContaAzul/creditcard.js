9a4c2b4ff59150b39c0ee8fe713d2021570e8c89
Luiz Fernando Rodrigues <fernahh@gmail.com>
2019-04-11 11:11:49 -0300

    Use a better directory structure

--- specs/creditCardList.spec.js
+++
@@ -1,9 +0,0 @@
-import CREDIT_CARD_LIST from '../src/creditCardList.js';
-
-describe('CreditCardList', () => {
-  describe('#getCreditCardList', () => {
-    it('should return a array with cards', () => {
-      expect(CREDIT_CARD_LIST instanceof Array).toBeTruthy();
-    });
-  });
-});
