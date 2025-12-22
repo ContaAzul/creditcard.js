e5971c563a7890017eee247f9407c16d02521f9c
Thiago Zanluca <zanlucathiago@gmail.com>
2025-12-22 17:34:05 -0300

    chore: update rollup and related plugins to latest versions

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
-import babel from 'rollup-plugin-babel';
-import { terser } from 'rollup-plugin-terser';
+import babel from '@rollup/plugin-babel';
+import terser from '@rollup/plugin-terser';
 import { version } from './package.json';
 
 const banner = `/**
@@ -23,12 +23,13 @@ export default {
       file: 'dist/creditcard.min.js',
       format: 'cjs',
       name: 'CreditCard',
-      plugins: [terser()],
+      plugins: [terser],
     },
   ],
   plugins: [
     babel({
       exclude: 'node_modules/**',
+      babelHelpers: 'bundled',
     }),
   ],
   watch: {
